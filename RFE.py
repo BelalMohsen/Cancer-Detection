1f32b54bc6fc749b42fc634404a2e5774d1035ad
Kumari Anamika Sharaf <kumarianamika.sharaf@sjsu.edu>
2017-06-22 22:14:30 -0700

    Add files via upload

---
+++ RFE.py
@@ -0,0 +1,20 @@
+# Feature Extraction with RFE
+from pandas import read_csv
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LogisticRegression
+import pdb
+
+# load data
+names = ['radius_mean', 'texture_mean', 'perimeter_mean', 'area_mean', 'smoothness_mean', 'compactness_mean', 'concavity_mean', 'concave_points_mean', 'symmetry_mean', 'fractal_dimension_mean', 'radius_se', 'texture_se', 'perimeter_se', 'area_se', 'smoothness_se', 'compactness_se', 'concavity_se', 'concave_points_se', 'symmetry_se', 'fractal_dimension_se', 'radius_worst', 'texture_worst', 'perimeter_worst', 'area_worst', 'smoothness_worst', 'compactness_worst', 'concavity_worst', 'concave_points_worst','symmetry_worst', 'fractal_dimension_worst']
+dataframe = read_csv("data.csv", names=names)
+array = dataframe.values
+X = array[1:,0:29]
+Y = array[1:,29]
+
+# feature extraction
+model = LogisticRegression()
+rfe = RFE(model, 2)
+fit = rfe.fit(X, Y)
+print("Num Features: %d") % fit.n_features_
+print("Selected Features: %s") % fit.support_
+print("Feature Ranking: %s") % fit.ranking_
